abd72a7a47bfa0541471fa1a8d37862b33059c7e
sergejjarosch <sergejjarosch@gmail.com>
2024-02-14 14:23:26 +0100

    add changes

--- src/App.java
+++ src/App.java
@@ -2,18 +2,9 @@
 
 public class App {
     public static void main(String[] args) {
+//--- AVG ---
+       // avg.avgEingabe.avg_input();
 
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-        System.out.print(" Enter numbers seperated by comas: ");
-        String input = scanner.nextLine();
-        String[] numbersAsString = input.split(",");
-        double [] numbers = new double[numbersAsString.length];
-
-        for ( int i =0; i < numbersAsString.length; i++)
-            numbers[i] = Double.parseDouble(numbersAsString[i].trim());
-
-        double average = avg.avgClass.avg_out(numbers);
-        System.out.print("Avg: " + average);
     }
 }
