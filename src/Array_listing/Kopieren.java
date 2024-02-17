c3a04a2b3474a61ba9d0b60f6b68e67866c270bb
sergejjarosch <sergejjarosch@gmail.com>
2024-02-17 12:21:46 +0100

    Add Array kopieren

--- src/Array_listing/Kopieren.java
+++ src/Array_listing/Kopieren.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 public class Kopieren {
     public static void kopieren_out(){
-        String [] names = {"Big Hunk", "Mamba", "Charms", "Gushers"};
+        String [] names = {"One", "Two", "Three", "Four"};
+
         String [] slice1= Arrays.copyOf( names, 2 );
-        System.out.println( Arrays.toString( slice1 ));
+        System.out.println( Arrays.toString( slice1 )); //<-- Output
+
         String [] slice2 = Arrays.copyOf( names, 5);
-        System.out.println( Arrays.toString( slice2 ) );
+        System.out.println( Arrays.toString( slice2 ) );//<-- Output
 
         String[] slice3 = Arrays.copyOfRange( names, 2, 4);
-        System.out.println( Arrays.toString( slice3 ) );
-        String[] slice4 = Arrays.copyOfRange( names, 2, 5);
-        System.out.println( Arrays.toString( slice4 ) );
+        System.out.println( Arrays.toString( slice3 ) );//<-- Output
 
+        String[] slice4 = Arrays.copyOfRange( names, 2, 5);
+        System.out.println( Arrays.toString( slice4 ) );//<-- Output
     }
+
 }
